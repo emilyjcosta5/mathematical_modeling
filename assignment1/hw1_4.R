437a733b94217d01a210aaf9056c52f5b0f7e34f
Emily Costa <ecost020@fiu.edu>
2019-09-08 18:56:37 -0400

    Completed third and fourth functions

--- assignment1/hw1_4.R
+++ assignment1/hw1_4.R
@@ -4,7 +4,7 @@
 
 # a. 2x n − 5x n−1 = 0, x 0 = 2.
 a <- function(iterations){
-        n <- seq(from=0,to=iterations,by=1)
+        n <- seq(from=0,to=iterations-1,by=1)
         x <- numeric(length(n))
         x[1] = 2
         for(a in seq_len(length(n)-1)){
@@ -15,21 +15,40 @@ a <- function(iterations){
 
 # b. 2q n − 5q n−1 = 3, q 0 = 3.
 b <- function(iterations){
-	n <- seq(from=0,to=iterations,by=1)
+	n <- seq(from=0,to=iterations-1,by=1)
 	q <- numeric(length(n))
 	q[1] = 3
-	for(a in seq_length(length(n)-1){
-		    q[a+1] = (3/2) + ((5/2)*q[a])
+	for(a in seq_len(length(n)-1)){
+		q[a+1] = (3/2) + ((5/2)*q[a])
         }
 	print(q)
 }
 
 # c. 2q n+1 − 7q n + 3q n−1 = 0, q 0 = 1, q 1 = 2.
 c <- function(iterations){
-	n <- seq(from=0,to=iterations,by=1)
+	n <- seq(from=0,to=iterations-1,by=1)
 	q <- numeric(length(n))
 	q[1] = 1
 	q[2] = 2
+	for(a in seq_len(length(n)-2)){
+		q[a+2] = ((7/2)*q[a+1]) - ((3/2)*q[a])
+        }
+	print(q)
+}
 
+# d. 2q n+1 − 7q n + 3q n−1 = 2 + 2 n , q 0 = 1, q 1 = 2.
+d <- function(iterations){
+	n <- seq(from=0,to=iterations-1,by=1)
+	q <- numeric(length(n))
+	q[1] = 1
+	q[2] = 2
+	for(a in seq_len(length(n)-2)){
+		q[a+2] = ((7/2)*q[a+1]) - ((3/2)*q[a]) + (2**a/2) + 1
+	}
+	print(q)
+}
 
-
+a(10)
+b(10)
+c(10)
+d(10)
